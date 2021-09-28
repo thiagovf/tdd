3528ad8f4a896cd1df0d5dc5f9a81ddfabc2c045
Thiago <4077420+thiagovf@users.noreply.github.com>
2021-09-28 10:00:46 -0300

    utilizando strategy com enum

--- src/main/java/br/com/tdd/modelo/Desempenho.java
+++ src/main/java/br/com/tdd/modelo/Desempenho.java
@@ -1,6 +1,24 @@
 package br.com.tdd.modelo;
 
-public enum Desempenho {
-	A_DESEJAR, BOM, OTIMO
+import java.math.BigDecimal;
 
+public enum Desempenho {
+	A_DESEJAR {
+		@Override
+		public BigDecimal percentualReajuste() {
+			return new BigDecimal("0.03");
+		}
+	}, BOM {
+		@Override
+		public BigDecimal percentualReajuste() {
+			return new BigDecimal("0.15");
+		}
+	}, OTIMO {
+		@Override
+		public BigDecimal percentualReajuste() {
+			return new BigDecimal("0.2");
+		}
+	};
+
+	public abstract BigDecimal percentualReajuste();
 }
--- src/main/java/br/com/tdd/service/ReajusteService.java
+++ src/main/java/br/com/tdd/service/ReajusteService.java
@@ -6,22 +6,8 @@ import br.com.tdd.modelo.Desempenho;
 import br.com.tdd.modelo.Funcionario;
 
 public class ReajusteService {
-
 	public void concederReajuste(Funcionario funcionario, Desempenho desempenho) {
-		BigDecimal percentual;
-		if (Desempenho.A_DESEJAR.equals(desempenho)) {
-			percentual = new BigDecimal("0.03");
-		} else if (Desempenho.BOM.equals(desempenho)) {
-			percentual = new BigDecimal("0.15");
-		} else if (Desempenho.OTIMO.equals(desempenho)) {
-			percentual = new BigDecimal("0.2");
-		} else {
-			percentual = new BigDecimal("0");
-		}
-		
-		BigDecimal reajuste = funcionario.getSalario().multiply(percentual);
+		BigDecimal reajuste = funcionario.getSalario().multiply(desempenho.percentualReajuste());
 		funcionario.reajustarSalario(reajuste);
-		
 	}
-
 }
